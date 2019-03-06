f91ba1ac663057b7b1f383c1d75ccf72b5c34b5f
chenxi1900 <gonghuiyun@buaa.edu.cn>
2019-03-06 13:48:55 +0800

    0101

---
+++ week1/1.py
@@ -0,0 +1,16 @@
+'''
+双指针(题号：167)：
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+'''
+def twoSum(numbers, target):
+    i = 0
+    j = len(numbers)-1-i
+    while not i == j:
+        if numbers[i] + numbers[j] <target:
+            i = i+1
+        elif numbers[i] + numbers[j] >target:
+            j = j-1
+        else:
+            return [i + 1, j + 1]
+
+print(twoSum([2,7,11,15],9))
